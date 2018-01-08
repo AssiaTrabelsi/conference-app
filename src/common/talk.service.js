878f3c066f43e663e75006292f3393743c60b83d
Asia Trabelsi <assia.trabelsi93@gmail.com>
2018-01-08 14:00:03 +0100

    Api-Rest

---
+++ src/common/talk.service.js
@@ -0,0 +1,32 @@
+
+export default class TalkService {
+
+    findAllSpeakers() {
+        return new Promise((resolve, reject) => {
+
+            const req = new XMLHttpRequest();
+
+            req.open('GET', 'http://localhost:3000/speakers', true);
+
+            req.onreadystatechange = (aEvt) => {
+                if (req.readyState == 4) {
+                    if (req.status == 200) {
+                        resolve(JSON.parse(req.responseText));
+
+                    } else {
+
+                        reject(`Erreur pendant le chargement de la page`)
+
+                    }
+
+                };
+
+            }
+
+            req.send(null);
+
+        })
+
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert('Conference App démarré!')
+console.log('Conference App démarré!')
+
+import TalkService from './common/talk.service'
+
+const talkService = new TalkService();
+
+talkService.findAllSpeakers().then(speakers => {
+    speakers.forEach(sp => {
+        console.log(sp.firstname)
+    })
+}
+
+)
+
